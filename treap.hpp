dc3c32a592a6d47fd058a0b3c590e2e793b50c8e
Sinapsel <marksimanovskiy@gmail.com>
2017-12-15 12:55:08 +0300

    Miracles fixed)

--- treap.hpp
+++ treap.hpp
@@ -1,408 +1,362 @@
-/******************************************************************************
-     * File:            treap.hpp
-     * Description:     The main header file with template Treap class
-     * Created:         10 sep 2017
-     * Copyright:       MIT License (C) 2017 Sinapsel
-     * Authors:         Gorbatov N., Simanovskiy M.        (sorted by alphabet)
-     * Email:           gorbatov.nickita@yandex.ru, marksimanovskiy@gmail.com
-     * GitHub:          https://github.com/sinapsel/treabble
-******************************************************************************/
-
-#ifndef TREAP_H_INCLUDED
-#define TREAP_H_INCLUDED
-
-#include <cstdlib>
+#ifndef TREAP_H
+#define TREAP_H
 #include <iostream>
-#include <algorithm>
-#include <time.h>
+using namespace std;
 
-namespace service{
-	template <class T1, class T2>
-	struct pair{
-		T1 first;
-		T2 second;
-		pair(const T1& f, const T2& s){
-			this->first = f;
-			this->second = s;
-		}
-		pair():first(0),second(0){}
-		template <class U, class V> pair(const pair<U,V>& pr){
-			(*this) = *pr;
-		}
-	};
-
-	template <typename T>
-	T max(T a, T b){
-		return ( a > b ) ? a : b;
-	}
-	template <typename T>
-	T min(T a, T b){
-		return ( a < b ) ? a : b;
-	}
-}
-
-
-/**
- * @name    treap
- * @author Gorbatov N., Simanovskiy M.             (sorted by alphabet)
- * @date    10 sep 2017
- * @version 0.2
- * @brief Abstract data type class with fast O(log2(n)) search, insert, erase and etc
- */
-template <class T>
-class treap{
-    public:
-        /**
-         * Constuctor
-         * @param val init value
-         */
-        treap(const T& val){
-            this->data = val;
-            srand(time(0));
-            this->y    = ((rand()<<15) + rand());
-            this->size = 1;
-            this->left = nullptr;
-            this->right= nullptr;
-            this->max  = val;
-            this->min  = val;
-            this->sum  = val;
-            this->add  = 0;
-            this->mpc  = 1;
-        }
-        ~treap(){
-            if(this->left)
-                delete this->left;
-            if(this->right)
-                delete this->right;
-        }
-        /**
-         * @return size of treap class instance
-         */
-        size_t get_size(){
-			if(this)
-				return this->size;
-			else
-				return (size_t)0;
-		}
-		/**
-		 * @param root instance of treap class
-         * @return size of root
-         */
-        static size_t get_size(treap * root){
-			if(root)
-				return root->size;
-			else
-				return (size_t)0;
-		}
-
-        size_t depth(){
-			if (this == nullptr)
-				return 0;
-			else
-				return 1 + service::max(this->left->depth(), this->right->depth());
-		}
-        /**
-         * @return true if key is in treap
-         * @param key searching for key in a treap
-         */
-        bool exists(T key){
-			if (this == nullptr)
-				return false;
-			if (key == this->data)
-				return true;
-			if (this->left->exists(key))
-				return true;
-			if (this->right->exists(key))
-				return true;
-			return false;
-		}
-        /**
-         * @param k position
-         * @return k-th element in the treap
-         */
-        T kth(size_t k){
-			if (k <= this->left->get_size())
-				return this->left->kth(k);
-			if (k == this->left->get_size()+1)
-				return this->data;
-			return this->right->kth(k - this->left->get_size() - 1);
-		}
-		treap<T> * kth_t(size_t k){
-            if (k <= this->left->get_size())
-				return this->left->kth_t(k);
-			if (k == this->left->get_size()+1)
-				return this;
-			return this->right->kth_t(k - this->left->get_size() - 1);
-		}
-        T get_max(){
-			if (this)
-				return this->add + this->max*this->mpc;
-			else
-				return -INF;
-		}
-        T get_min(){
-			if (this)
-				return this->add + this->min*this->mpc;
-			else
-				return INF;
-		}
-        T get_sum(){
-			if (this)
-				return this->sum * this->mpc - this->add * this->size;
-			else
-				return 0;
-		}
-        static T get_max(treap * root){
-			if (root)
-				return root->add + root->max*root->mpc;
-			else
-				return -INF;
-		}
-        static T get_min(treap * root){
-			if (root)
-				return root->add + root->min*root->mpc;
-			else
-				return INF;
-		}
-        static T get_sum(treap * root){
-			if (root)
-				return root->sum * root->mpc - root->add * root->size;
-			else
-				return 0;
-		}
-
-        /**
-         * @param root1 first treap
-         * @param root2 second treap
-         * @return merged treap contains of two treaps
-         */
-        static treap<T> * merge (treap<T> * root1, treap<T> * root2){
-			if (root1 == nullptr)
-				return root2;
-			if (root2 == nullptr)
-				return root1;
-			if (root1->y < root2->y) {
-				root1->push();
-				root1->right = merge(root1->right, root2);
-				root1->update();
-				return root1;
-			}
-			else {
-				root2->push();
-				root2->left = merge(root1, root2->left);
-				root2->update();
-				return root2;
-			}
-		}
-		/**
-         * @param root2 second treap
-         * @return merged treap contains of instance and @param root2
-         */
-		/*treap<T> * merge (treap<T> * root2){
-			if (this == nullptr)
-				return root2;
-			if (root2 == nullptr)
-				return this;
-			if (this->y < root2->y) {
-				this->push();
-				this->right = this->right->merge(root2);
-				this->update();
-				return this;
-			}
-			else {
-				root2->push();
-				root2->left = this->merge(root2->left);
-				root2->update();
-				return root2;
-			}
-		}
-		*/
-
-		/**
-		 * @brief Inserts the value into position
-		 * @param pos position where to insert a value
-		 * @param val the value
-		 */
-        treap<T> * insert(size_t pos, T val){
-            //service::pair<treap<T> *, <treap<T> *> res = this->split_kth(pos - 1);
-            auto res = this->split_kth(pos - 1);
-            treap<T> * ntreap = new treap<T>(val);
-            //*this = *((res.first->merge(ntreap))->merge(res.second));
-            //return (res.first->merge(ntreap))->merge(res.second);
-            return merge(merge(res.first, ntreap), res.second);
-        }
-        /**
-         * @brief Deletes elemento on position
-         * @param pos position of element to delete
-         */
-        treap<T> * erase (size_t pos){
-            auto res1 = this->split_kth(pos);
-            auto res2 = res1.first->split_kth( pos - 1);
-            delete res2.second;
-            return merge(res2.first, res1.second);
-        }
-        /**
-         * @brief splits treap on k position to pair of left and right treaps
-         * @param k position
-         * @return service::pair of left and right treaps
-         */
-        service::pair <treap<T> *, treap<T> *> split_kth(size_t k){
-            if (this == nullptr)
-                return{ nullptr,nullptr };
-            if (get_size() <= k)
-                return{ this,nullptr };
-            if (k == 0)
-                return{ nullptr,this };
-            this->push();
-            if (k <= get_size(this->left)) {
-                auto res = this->left->split_kth( k);
-                this->left = res.second;
-                this->update();
-                return{ res.first,this };
-            }
-            else {
-                auto res = this->right->split_kth( k - 1 - this->left->get_size());
-                this->right = res.first;
-                this->update();
-                return{ this,res.second };
-            }
-        }
-        /**
-         * @brief Group operations
-         * @param op operation (+, -, *)
-         * @param left, right pos'es
-         */
-        void gr_op(char op, T val, size_t left, size_t right) {
-            auto  res1 = this->split_kth(right);
-            auto  res2 = res1.first->split_kth(left - 1);
-            if (op == '+')
-                res2.second->add += val;
-            if (op == '-')
-                res2.second->add -= val;
-            if (op == '*') {
-                res2.second->mpc *= val;
-                res2.second->add *= val;
-            }
-
-            merge(merge(res2.first, res2.second), res1.second);
-        }
-        /**
-         * @brief Group Query
-         * @param query string with action to do {min|max|sum}
-         * @param left, right pos'es
-         * @return result of Group Query
-         */
-        T gr_qu(const char* query, size_t left, size_t right) {
-            auto res1 = this->split_kth(right);
-            auto res2 = res1.first->split_kth(left - 1);
-            T ans = 0;
-            if (*query == *"min") {
-                ans = res2.second->min;
-            }
-            if (*query == *"max") {
-                ans = res2.second->max;
-             }
-            if (*query == *"sum") {
-                ans = res2.second->sum;
-            }
-            merge(merge(res2.first, res2.second), res1.second);
-            return ans;
-        }
-
-        /**
-         * @brief prints all elements of treap each on next line
-         */
-        void print(){
-			if (this == nullptr)
-				return;
-			this->push();
-			this->left->print();
-			std::cout <<this->add + this->data * this->mpc << std::endl;
-			this->right->print();
-		}
-		/**
-         * @brief prints all elements of treap with a separator after each one
-         * @param sep string with separator
-         */
-		void print(const char* sep){
-			if (this == nullptr)
-				return;
-			this->push();
-			this->left->print(sep);
-            std::cout <<this->add + this->data * this->mpc << sep;
-
-            this->right->print(sep);
-		}
-
-		T operator[](size_t n){
-            auto a = (this->kth(n));
-            return a->data;
-		}
-
-
-        treap<T> * get_left(){
-            if(this->left != nullptr)
-                return this->left;
-            else
-                return 0;
-        }
-        treap<T> * get_right(){
-            if(this->right != nullptr)
-                return this->right;
-            else
-                return 0;
-        }
-        long getY(){
-            return this->y;
-        }
-        T get_data(){
-            if(this != nullptr)
-                return this->data;
-            else
-                return 0;
-        }
-
-    private:
-        size_t size;
-        treap * left;
-        treap * right;
-
-        long y;
-
-        T data;
-        T max, min;
-        T sum;
-        T mpc, add;
-
-        static const int INF = 1e9;
-
-
-        void update(){
-            if (!this)
-                return;
-            this->size = 1 + this->left->get_size() + this->right->get_size();
-            this->max = service::max(this->data, service::max(this->left->get_max(), this->right->get_max()));
-            this->min = service::min(this->data, service::min(this->left->get_min(), this->right->get_min()));
-            this->sum = this->left->get_sum() + this->right->get_sum() + this->data;
-        }
-
-        void push(){
-            if (this->left) {
-                this->left->add += this->add;
-                this->left->mpc *= this->mpc;
-            }
-            if (this->right) {
-                this->right->add += this->add;
-                this->right->mpc *= this->mpc;
-            }
-            this->data = this->add + this->data*this->mpc;
-            this->max = this->add + this->max*this->mpc;
-            this->min = this->add + this->min*this->mpc;
-            this->sum = this->sum * this->mpc - this->add * this->size;
-            this->add = 0;
-            this->mpc = 1;
-        }
-
+namespace service {
+typedef long big;
+const big INF = 1e9;
+template <class T1, class T2>
+struct pair {
+  T1 first;
+  T2 second;
+  pair(const T1 &f, const T2 &s) {
+    this->first = f;
+    this->second = s;
+  }
+  pair() : first(0), second(0) {}
+  template <class U, class V>
+  pair(const pair<U, V> &pr) {
+    (*this) = *pr;
+  }
 };
 
-#endif // TREAP_H_INCLUDED
+template <typename T>
+T max(T a, T b) {
+  return (a > b) ? a : b;
+}
+template <typename T>
+T min(T a, T b) {
+  return (a < b) ? a : b;
+}
+}
+
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+template <class T>
+class treap {
+  //#define cnt(t) ((t)?(t)->c:0)
+  //    #define update(t) if(t)(t)->c=1+cnt(t->l)+cnt(t->r)
+  struct node {
+    T val;
+    size_t key, c;
+
+    T max, min, sum, size, add, mpc;
+    bool rev;
+
+    node *left, *right;
+    static size_t cnt(node *t) { return ((t) ? (t)->c : 0); }
+    node() {
+      left = right = nullptr;
+      c = 0;
+      rev = false;
+      key = rand() << 15 | rand();
+      add = 0;
+      mpc = 1;
+    }
+    node(T V, node *L, node *R) : val(V) {
+      key = rand() << 15 | rand();
+      left = L;
+      right = R;
+      c = 1 + cnt(left) + cnt(right);
+      rev = false;
+      add = 0;
+      mpc = 1;
+    }
+  };
+
+  node *root;
+  int n;
+
+  void push(node *&root) {
+    if (root && root->rev) {
+      root->rev = false;
+      node *q = root->left;
+      root->left = root->right;
+      root->right = q;
+      if (root->left) {
+        root->left->rev ^= true;
+        root->left->add += root->add;
+        root->left->mpc *= root->mpc;
+      }
+      if (root->right) {
+        root->right->rev ^= true;
+        root->right->add += root->add;
+        root->right->mpc *= root->mpc;
+      }
+      root->val = root->add + root->val * root->mpc;
+      root->max = root->add + root->max * root->mpc;
+      root->min = root->add + root->min * root->mpc;
+      root->sum = root->sum * root->mpc - root->add * root->size;
+      root->add = 0;
+      root->mpc = 1;
+    }
+  }
+
+  int size(node *root) {
+    if (root) {
+      return root->size;
+    } else {
+      return 0;
+    }
+  }
+
+  void update(node *root) {
+    if (!root) return;
+    root->c = 1 + node::cnt(root->left) + node::cnt(root->right);
+    root->size = 1 + size(root->left) + size(root->right);
+    root->max =
+        service::max(root->val, max(get_max(root->left), get_max(root->right)));
+    root->min =
+        service::min(root->val, min(get_min(root->left), get_min(root->right)));
+    root->sum = get_sum(root->left) + get_sum(root->right) + root->val;
+  }
+
+  void merge(node *&t, node *left, node *right) {
+    push(left);
+    push(right);
+    if (!left)
+      t = right;
+    else if (!right)
+      t = left;
+    else if (left->key > right->key) {
+      merge(left->right, left->right, right);
+      t = left;
+    } else {
+      merge(right->left, left, right->left);
+      t = right;
+    }
+    update(t);
+  }
+
+  void split(node *t, int sk, node *&left, node *&right, int less = 0) {
+    if (!t)
+      left = right = nullptr;
+    else {
+      push(t);
+      int key = node::cnt(t->left) + less;
+      if (sk <= key) {
+        split(t->left, sk, left, t->left, less);
+        right = t;
+      } else {
+        split(t->right, sk, t->right, right, less + node::cnt(t->left) + 1);
+        left = t;
+      }
+      update(t);
+    }
+  }
+
+  void print(node *&t) {
+    if (!t) return;
+    push(t);
+    print(t->left);
+    cout << t->val << ' ';
+    print(t->right);
+  }
+
+  bool isexist(node *root, T key) {
+    if (!root) return false;
+    if (key == root->val) return true;
+    if (isexist(root->left, key)) return true;
+    if (isexist(root->right, key)) return true;
+    return false;
+  }
+
+  T *find(node *root, T elem) {
+    if (root == nullptr) return nullptr;
+    if (elem == root->val)
+      return (&root->val);
+    else {
+      T *l = find(root->left, elem);
+      if (l == nullptr) {
+        l = find(root->right, elem);
+        if (l == nullptr)
+          return nullptr;
+        else
+          return l;
+      } else
+        return l;
+    }
+  }
+
+  T get_max(node *root) {
+    if (root)
+      return root->add + root->max * root->mpc;
+    else
+      return -service::INF;
+  }
+
+  T get_min(node *root) {
+    if (root)
+      return root->add + root->min * root->mpc;
+    else
+      return service::INF;
+  }
+
+  T get_sum(node *root) {
+    if (root)
+      return root->sum * root->mpc - root->add * root->size;
+    else
+      return 0;
+  }
+
+  int depth(node *root) {
+    if (root == nullptr) return 0;
+    return 1 + service::max(depth(root->left), depth(root->right));
+  }
+
+  void gr_op(char op, int val, node *root, int left, int right) {
+    auto res1 = split_kth(root, right);
+    auto res2 = split_kth(res1.first, left - 1);
+    if (op == '+') res2.second->add += val;
+    if (op == '-') res2.second->add -= val;
+    if (op == '*') {
+      res2.second->mpc *= val;
+      res2.second->add *= val;
+    }
+    merge(merge(res2.first, res2.second), res1.second);
+  }
+
+  int gr_qu(char query, node *root, int left, int right) {
+    auto res1 = split_kth(root, right);
+    auto res2 = split_kth(res1.first, left - 1);
+    int ans = 0;
+    if (query == 'n') {
+      int ans = res2.second->min;
+      merge(merge(res2.first, res2.second), res1.second);
+    }
+    if (query == 'x') {
+      int ans = res2.second->max;
+      merge(merge(res2.first, res2.second), res1.second);
+    }
+    if (query == 's') {
+      int ans = res2.second->sum;
+      merge(merge(res2.first, res2.second), res1.second);
+    }
+    return ans;
+  }
+
+ public:
+  treap() {
+    root = nullptr;
+    n = 0;
+  }
+  treap(node *n) { root = n; }
+  treap(T val) {
+    root = nullptr;
+    n = 0;
+    this.val = val;
+  }
+  treap(vector<T> &a) {
+    root = nullptr;
+    n = a.size();
+    for (auto it = a.begin(); it != a.end(); it++) this.insert(*it);
+  }
+
+  // T gLeftData(){return root->left->val;}
+  // T gRightData(){return root->right->val;}
+  // T gValue(){return root->val;}
+
+  void insert(int pos, T val) {
+    if (pos < 0 || pos > n) return;
+    node *q = new node(val, nullptr, nullptr);
+    node *tl, *tr;
+    split(root, pos, tl, tr);
+    merge(tl, tl, q);
+    merge(root, tl, tr);
+    ++n;
+  }
+
+  T &at(int pos) {
+    node *t = root;
+    int less = 0;
+    while (t) {
+      int key = cnt(t->left) + less;
+      if (pos == key) break;
+      if (pos < key)
+        t = t->left;
+      else {
+        less += cnt(t->left) + 1;
+        t = t->r;
+      }
+    }
+    return t->val;
+  }
+
+  T &operator[](int pos) {
+    node *t = root;
+    int less = 0;
+    while (t) {
+      int key = node::cnt(t->left) + less;
+      if (pos == key) break;
+      if (pos < key)
+        t = t->left;
+      else {
+        less += node::cnt(t->left) + 1;
+        t = t->right;
+      }
+    }
+    return t->val;
+  }
+
+  void reverse(int left, int right) {
+    left = service::max(left, 0);
+    right = service::min(right, n - 1);
+    if (left > right) return;
+
+    node *tl, *tm, *tr;
+    split(root, left, tl, tr);
+    split(tr, right - left + 1, tm, tr);
+    tm->rev ^= true;
+    merge(tl, tl, tm);
+    merge(root, tl, tr);
+  }
+
+  void move(int left, int right, int w) {
+    if (w == 0 || left > right || left + w < 0 || right + w >= n) return;
+
+    node *tl, *tm, *tr;
+    split(root, left, tl, tr);
+    split(tr, right - left + 1, tm, tr);
+    merge(tl, tl, tr);
+    split(tl, left + w, tl, tr);
+    merge(tl, tl, tm);
+    merge(root, tl, tr);
+  }
+
+  void erase(int left, int right) {
+    left = service::max(left, 0);
+    right = service::min(right, n - 1);
+    if (left > right) return;
+
+    node *tl, *tm, *tr;
+    split(root, left, tl, tr);
+    split(tr, right - left + 1, tm, tr);
+    merge(root, tl, tr);
+    n -= (right - left + 1);
+  }
+
+  T *find(T elem) { return find(root, elem); }
+
+  T get_max() { return get_max(root); }
+
+  T get_min() { return get_min(root); }
+
+  T get_sum() { return get_sum(root); }
+
+  bool isexist(T key) { return isexist(root, key); }
+
+  int depth() { return depth(root); }
+
+  size_t size() { return n; }
+
+  void print() {
+    print(root);
+    cout << endl;
+  }
+};
+
+#endif  // TREAP_H
